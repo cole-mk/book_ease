fe4e9b83f18ef74d997c9802e7ffdf7df67e33b0
mark <mark@capstonedistribution.com>
2022-04-09 12:50:41 -0500

    move signal_.py to top level of src directory

---
+++ src/signal_.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#  signal_.py
+#
+#  Copyright 2020 mark <mark@capstonedistribution.com>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+"""
+helper class for model classes (MVC) to implement signal system (notifications)
+"""
+
+class Signal_():
+
+    def __init__(self):
+        # each entry will hold a list of tuples as defined in connect()
+        self._sig_handlers = {}
+
+    def add_signal(self, handle):
+        # add signal to the sig handlers list
+        # called by server
+        self._sig_handlers[handle] = []
+
+    def remove_signal(self, handle):
+        # remove signal from the sig handlers list
+        # called by server
+       del self._sig_handlers[handle]
+
+    def connect(self, handle, method, *args, **cb_kwargs):
+        # connect callback to signal in the sig handlers list
+        # called by client
+        self._sig_handlers[handle].append((handle, method, args, cb_kwargs))
+
+    def signal(self, handle):
+        # execute each signal in the sig handlers list
+        # called by server
+        [signal[1](*signal[2], **signal[3]) for signal in self._sig_handlers[handle]]
