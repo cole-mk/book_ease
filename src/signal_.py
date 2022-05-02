70349048dda89da309b6da85b442ad37e7bf0784
mark <mark@capstonedistribution.com>
2022-05-02 15:48:11 -0500

    closes #2 add or edit correct gplv3 licensing info at top of all src files
    Signed-off-by: mark <mark@capstonedistribution.com>

--- src/signal_.py
+++ src/signal_.py
@@ -3,11 +3,24 @@
 #
 #  signal_.py
 #
-#  Copyright 2020 mark <mark@capstonedistribution.com>
+#  Copyright 2020 mark cole <mark@capstonedistribution.com>
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
+#  the Free Software Foundation; either version 3 of the License, or
+#  (at your option) any later version.
+#
+# -*- coding: utf-8 -*-
+#
+#  signal_.py
+#
+#  This file is part of book_ease.
+#
+#  Copyright 2021 mark cole <mark@capstonedistribution.com>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3 of the License, or
 #  (at your option) any later version.
 #
 #  This program is distributed in the hope that it will be useful,
