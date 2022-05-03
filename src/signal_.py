4e156e5ed5d1f754e4e55352cc865f51927e5e47
mark <mark@capstonedistribution.com>
2022-05-02 20:02:47 -0500

    delete old license info that was missed on #2

--- src/signal_.py
+++ src/signal_.py
@@ -1,15 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-#
-#  signal_.py
-#
-#  Copyright 2020 mark cole <mark@capstonedistribution.com>
-#
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 3 of the License, or
-#  (at your option) any later version.
-#
 # -*- coding: utf-8 -*-
 #
 #  signal_.py
