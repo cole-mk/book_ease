0e654699ecb251a47b65b2981f00cba004627aba
cole-mk <65479257+cole-mk@users.noreply.github.com>
2022-08-13 11:50:49 -0500

    287 create module sqlite tools (#288)
    * create file sqlite_tools.py
    
    * add class DBConnectionManagerto sqlite_tools.py closes #287
    
    move DBConnection class to sqlite_tools and rename to
    DBConnectionManager.
    
    merge create_connection into DBConnectionManager.
    
    adjust the constructor to require a database file.

---
+++ src/sqlite_tools.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+#
+#  sqlite_tools.py
+#
+#  This file is part of book_ease.
+#
+#  Copyright 2021 mark cole <mark@capstonedistribution.com>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+"""This module contains various helper classes specific to using an sqlite db."""
+import sqlite3
+from pathlib import Path
+
+
+class DBConnectionManager:
+    """provide database connection management for multi queries"""
+
+    def __init__(self, database: Path):
+        self.database = database
+        self.con = None
+
+    def create_connection(self) -> sqlite3.Connection:
+        """ create a sqlite3 connection object and return it"""
+        con = sqlite3.connect(self.database, isolation_level=None)
+        con.row_factory = sqlite3.Row
+        return con
+
+    def multi_query_begin(self):
+        """
+        create a semi-persistent connection
+        for executing multiple transactions
+        """
+        if self.con is not None:
+            raise RuntimeError('connection already exists')
+        self.con = self.create_connection()
+        self.con.execute('BEGIN')
+
+    def multi_query_end(self):
+        """commit and close connection of a multi_query"""
+        if self.con is None:
+            raise RuntimeError('connection doesn\'t exist')
+        self.con.commit()
+        self.con.close()
+        self.con = None
+
+    def query_begin(self) -> sqlite3.Connection:
+        """
+        get an sqlite connection object
+        returns self.con if a multi_query is in effect.
+        Otherwise, create and return a new connection
+        """
+        if self.con is None:
+            return self.create_connection()
+        return self.con
+
+    def query_end(self, con: sqlite3.Connection):
+        """
+        commit and close connection if a multi_query
+        is not in effect.
+        """
+        if con is not self.con:
+            con.commit()
+            con.close()
