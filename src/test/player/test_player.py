a87049cd2e4f2448ed1d24237e73f61d2aeea150
mark <mark@capstonedistribution.com>
2022-10-31 19:09:00 -0500

    create file test/player/test_player.py #362
    This commit includes the preamble, pylint directives, and imports.

---
+++ src/test/player/test_player.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+#
+#  test_player.py
+#
+#  This file is part of book_ease.
+#
+#  Copyright 2021 mark cole <mark@capstonedistribution.com>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
+# pylint: disable=invalid-name
+# disabled because in the IDE project structure sidebar, I want the test classes sorted in the same order
+# as the methods they are testing.
+#
+# pylint: disable=protected-access
+# disabled because this module is testing protected methods.
+#
+# pylint: disable=too-few-public-methods
+# disabled because some of the tested methods only require one test.
+#
+# pylint: disable=comparison-with-callable
+# disabled because this needs to be done regularly during tests.
+#
+
+"""
+Unit test for class player.Player
+"""
+
+from unittest import mock
+import pytest
+import player
+from player import GstPlayer
