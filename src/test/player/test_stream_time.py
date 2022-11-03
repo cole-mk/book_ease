a7b2eed3491afcf53832ce3f2fb744a3d2c6b98a
mark <mark@capstonedistribution.com>
2022-11-03 13:31:35 -0500

    Create file test/player/test_stream_time.py

---
+++ src/test/player/test_stream_time.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+#
+#  test_stream_time.py
+#
+#  This file is part of book_ease.
+#
+#  Copyright 2021 mark cole <mark@capstonedistribution.com>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
+# pylint: disable=wrong-import-position
+# disabled because gi.repository requires an import order that pylint dislikes.
+#
+# pylint: disable=invalid-name
+# disabled because in the IDE project structure sidebar, I want the test classes sorted in the same order
+# as the methods they are testing.
+#
+# pylint: disable=protected-access
+# disabled because this module is testing protected methods.
+#
+# pylint: disable=too-few-public-methods
+# disabled because some of the tested methods only require one test.
+#
+# pylint: disable=comparison-with-callable
+# disabled because this needs to be done regularly during tests.
+#
+# pylint: disable=too-many-lines
+# disabled because this needs to be as long as it needs to be.
+#
+
+"""
+Unit test for class player.StreamTime
+"""
+
+from player import StreamTime
